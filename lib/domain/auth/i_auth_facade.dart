758515ef2bdc468c7fdd50e85fc9f1ba631d70a0
Krzysztof Rosiek <k.rosiek@emys.co>
2021-05-16 11:43:23 +0200

    Adding fasade for sign-in methods.

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+import 'package:dddcourse/domain/auth/auth_failure.dart';
+import 'package:flutter/foundation.dart';
+import 'value_objects.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+  Future<void> signOut();
+}
